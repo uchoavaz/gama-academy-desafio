02cd8f6457df0e69c58b7bed16cc4fc3f8de767b
joao uchoa <joaouchoavaz@hotmail.com>
2021-07-16 21:57:30 -0300

    (=)Change script exec path

--- apply_destroy_script.sh
+++ apply_destroy_script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 CMD=$(cat terraform_command)
-
+cd infra
 if [ "$CMD" == "apply" ]
 then
 
-terraform apply -var-file="infra/envs/desafio.tfvars"
+terraform apply -var-file="envs/desafio.tfvars"
 
 elif [ "$CMD" == "destroy" ]
 then
 
-terraform destroy -var-file="infra/envs/desafio.tfvars"
+terraform destroy -var-file="envs/desafio.tfvars"
 
 fi
